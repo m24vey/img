eaa69757b18958ce4cd3456396679607e742c3ac
m24vey <108983311+m24vey@users.noreply.github.com>
2022-08-06 13:56:02 +0700

    Add files via upload

---
+++ sql_2.php
@@ -0,0 +1,10 @@
+GIF89a;
+<?php
+
+	$conn = new mysqli(getenv('MYSQL_HOSTNAME'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), getenv('MYSQL_DATABASE'));
+	SELECT table_name FROM information_schema.tables WHERE table_schema = database();
+	// SELECT * FROM tonghop;
+
+
+?>
+
