40d28815fece817881662491de8cd107fa1b6b02
m24vey <108983311+m24vey@users.noreply.github.com>
2022-08-06 13:51:01 +0700

    Update sql.php

--- sql.php
+++ sql.php
@@ -2,8 +2,8 @@ GIF89a;
 <?php
 
 	$conn = new mysqli(getenv('MYSQL_HOSTNAME'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), getenv('MYSQL_DATABASE'));
-	show tables;
-	SELECT * FROM tonghop;
+	SELECT table_name FROM information_schema.tables WHERE table_schema = database();
+	//SELECT * FROM tonghop;
 
 
 ?>
